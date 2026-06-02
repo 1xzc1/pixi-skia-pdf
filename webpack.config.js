2bf3e6abca8240d7fe66c0f4c0c5484b3d105a7e
Bugaev Egor <e.bugaev@turbocontract.ru>
2026-06-02 22:18:52 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     entry: './src/index.ts',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
         clean: true,
     },
